965a493f1e727a580848b0bc44335f3412da8814
Nugar <cladeadenugar@gmail.com>
2024-10-17 15:43:32 +0100

    new

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,36 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const items = ["Home", "Services", "Portfolio", "Contact"];
+  const variants = {
+    open: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+
+    closed: {
+      transition: {
+        staggerChildren: 0.05,
+        staggerDirection: -1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    open: {
+      y: 0,
+      opacity: 1,
+    },
+
+    closed: {
+      y: 50,
+      opacity: 0,
+    },
+  };
   return (
     <div>
       <motion.div
@@ -14,16 +45,72 @@ const Navbar = () => {
         }}
         transition={{ ease: "easeIn", duration: 1.5 }}
       >
-        <motion.a href="/" className="text-2xl">
+        <motion.a
+          href="/"
+          whileHover={{
+            scale: 1.2,
+            color: "lightblue",
+            rotate: 180,
+          }}
+          className="text-2xl"
+        >
           Nugar.dev
         </motion.a>
+        {/* mobile toggleButton */}
+        <motion.button
+          className="md:hidden"
+          onClick={() => setOpen((prev: any) => !prev)}
+          whileHover={{ scale: 1.5 }}
+        >
+          <motion.div
+            key={open ? "closeIcon" : "menuIcon"}
+            initial={{ opacity: 0, rotate: 90 }}
+            animate={{ opacity: 1, rotate: 0 }}
+            exit={{ opacity: 0, rotate: -90 }}
+            transition={{ duration: 0.3 }}
+          >
+            {open ? <CloseIcon /> : <MenuIcon />}
+          </motion.div>
+        </motion.button>
+
+        {/* sidebar menu */}
+        <motion.div className="hidden" variants={variants}>
+          {items.map((item) => (
+            <motion.a
+              href={`#${item}`}
+              key={item}
+              variants={itemVariants}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              {item}
+            </motion.a>
+          ))}
+        </motion.div>
 
         {/* desktop menu */}
-        <motion.div className="flex space-x-10 text-lg">
-          <motion.a href="/">Home</motion.a>
-          <motion.a href="#Services">Services</motion.a>
-          <motion.a href="#Works">Works</motion.a>
-          <motion.a href="#Contact">Contact</motion.a>
+        <motion.div className="md:flex space-x-10 text-lg hidden">
+          <motion.a href="/" whileHover={{ scale: 1.2, color: "lightblue" }}>
+            Home
+          </motion.a>
+          <motion.a
+            href="#Services"
+            whileHover={{ scale: 1.2, color: "lightblue" }}
+          >
+            Services
+          </motion.a>
+          <motion.a
+            href="#Works"
+            whileHover={{ scale: 1.2, color: "lightblue" }}
+          >
+            Works
+          </motion.a>
+          <motion.a
+            href="#Contact"
+            whileHover={{ scale: 1.2, color: "lightblue" }}
+          >
+            Contact
+          </motion.a>
         </motion.div>
       </motion.div>
     </div>
